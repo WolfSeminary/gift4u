c4662878f1469e6c6242a9c0061942bbb4e500d8
chaniZilber <chani59942@gmail.com>
2022-01-02 15:59:28 +0200

    secondVersion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import GiftCategories from './GiftCategories';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
-        </p>
-      </header>
+      <GiftCategories />
     </div>
   );
 }
