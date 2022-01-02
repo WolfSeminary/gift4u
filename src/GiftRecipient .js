11db5e279b11bc456437bd8a9c8e0f553215d8e1
SariZaretzki <sari127467@gmail.com>
2022-01-02 17:53:10 +0200

    AddGiftRecipientComponent

---
+++ src/GiftRecipient .js
@@ -0,0 +1,22 @@
+import Box from '@mui/material/Box';
+import Label from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+
+function GiftRecipient () {
+  return (
+      <>
+      <Label>Recipient name</Label>
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic"  variant="outlined" />
+    </Box>
+    </>
+  );
+}
+export default GiftRecipient 
