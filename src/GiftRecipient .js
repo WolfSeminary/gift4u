0e28bf155747b0cb94d7470f4aacf2c137e2c6ce
SariZaretzki <sari127467@gmail.com>
2022-01-16 16:25:21 +0200

    fix(GiftRecipient) fix spaces

--- src/GiftRecipient .js
+++ src/GiftRecipient .js
@@ -2,21 +2,22 @@ import Box from '@mui/material/Box';
 import Label from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 
-function GiftRecipient () {
+function GiftRecipient() {
   return (
-      <>
+    <>
       <Label>Recipient name</Label>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic"  variant="outlined" />
-    </Box>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" variant="outlined" />
+      </Box>
     </>
   );
 }
+
 export default GiftRecipient 
