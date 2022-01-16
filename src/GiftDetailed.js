f17fa00ebdbb3d0c3e2574dcc99145e6a63baaa4
CohenAyala <ayala40565@gmail.com>
2022-01-16 17:55:21 +0200

    fix(GiftDetailed)

--- src/GiftDetailed.js
+++ src/GiftDetailed.js
@@ -1,55 +1,30 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, Button, Typography, Box } from '@mui/material';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-const GiftDetailed =()=> {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+const GiftDetailed = () => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-        <Typography component="div">  
+      <Typography component="div">
         <Box sx={{ textAlign: 'right', m: 1 }}>
-            <CardHeader title="price"/></Box>
+          <CardHeader title="gift price" /></Box>
       </Typography>
       <CardMedia
-        component="img"
+        component=""
         height="194"
-        image=""
-        alt=""
+        image="url"
+        alt="gift image"
       />
       <CardContent>
-        <Typography variant="body2" color="text.secondary">        
+        <Typography variant="body2" color="text.secondary">gift name
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-      <Button id="btn" variant="contained" disableElevation>
-      CHOOSE
-    </Button>
+        <Button variant="contained" disableElevation>
+          CHOOSE
+        </Button>
       </CardActions>
     </Card>
   );
 }
+
 export default GiftDetailed;
