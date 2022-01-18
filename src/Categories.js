63566cc85b2c6b0e7ce7a4d6f1af725c81be1b84
SariZaretzki <96003501+SariZaretzki@users.noreply.github.com>
2022-01-18 18:18:29 +0200

    Update src/Categories.js
    change rhe parameters into string
    
    Co-authored-by: Eini Rachel Chaya <58681300+eini-rc@users.noreply.github.com>

--- src/Categories.js
+++ src/Categories.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const Categories =()=>{
   const [Categories,setCategories]=useState([
-    {CategoryID: 1, CategoryName: Food, categoryImage: <Image url="./images/abc-block.png"/>},
+    {CategoryID: 1, CategoryName: 'Food', categoryImage: './images/abc-block.png'/>},
     {CategoryID: 2, CategoryName: Attractions, categoryImage: <Image url="./images/amusement-park.png"/>},
     {CategoryID: 3, CategoryName: GiftCards, categoryImage: <Image url="./images/fast-food.png"/>},
     {CategoryID: 4, CategoryName: Toys, categoryImage: <Image url="./images/giftbox.png"/>}
