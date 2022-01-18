40ee0fe8917bcb068bd725c4e1197d126facc0cc
YehuditShw <91978643+YehuditShw@users.noreply.github.com>
2022-01-18 18:27:51 +0200

    change variables to string variables

--- src/Categories.js
+++ src/Categories.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
-const Categories =()=>{
-  const [Categories,setCategories]=useState([
-    {CategoryID: 1, CategoryName: 'Food', categoryImage: './images/abc-block.png'/>},
-    {CategoryID: 2, CategoryName: Attractions, categoryImage: <Image url="./images/amusement-park.png"/>},
-    {CategoryID: 3, CategoryName: GiftCards, categoryImage: <Image url="./images/fast-food.png"/>},
-    {CategoryID: 4, CategoryName: Toys, categoryImage: <Image url="./images/giftbox.png"/>}
+const Categories = () => {
+  const [Categories, setCategories] = useState([
+    { CategoryID: 1, CategoryName: 'Food', categoryImage: './images/abc-block.png' },
+    { CategoryID: 2, CategoryName: 'Attractions', categoryImage: "./images/amusement-park.png" },
+    { CategoryID: 3, CategoryName: 'GiftCards', categoryImage: "./images/fast-food.png" },
+    { CategoryID: 4, CategoryName: 'Toys', categoryImage: "./images/giftbox.png" }
   ]);
 }
 
