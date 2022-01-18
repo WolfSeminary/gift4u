6d7d5b7367232d515f6663b00cce18c70af5bb52
SariZaretzki <96003501+SariZaretzki@users.noreply.github.com>
2022-01-18 18:21:09 +0200

    Update src/FindGift.js
    change let variable to const variable
    
    Co-authored-by: Eini Rachel Chaya <58681300+eini-rc@users.noreply.github.com>

--- src/FindGift.js
+++ src/FindGift.js
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
 
 const FindGift = () => {
-    let navigate = useNavigate();
+    const navigate = useNavigate();
     return <Button onClick={()=>navigate('/categories')} variant="contained">Find a Gift</Button>
 }
 
