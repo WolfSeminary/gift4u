a37602083d1f650f64a84216411c42dc5436514c
SariZaretzki <sari127467@gmail.com>
2022-01-23 17:46:37 +0200

    fix:move the states the BuyGift page

--- src/BuyGiftPage.js
+++ src/BuyGiftPage.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import AppBar from './AppBar'
 import WhoGetTheGift from './WhoGetTheGift'
 import GiftRecipient from './GiftRecipient'
@@ -6,13 +6,17 @@ import GiftSender from './GiftSender'
 import GiftNote from './GiftNote'
 
 export default BuyGiftPage = () => {
+    const [whoGetsTheGift, setWhoGetsTheGift] = useState("a friend");
+    const [giftRecipient, setGiftRecipient] = useState();
+    const [giftSender, setGiftSender] = useState();
+    const [giftNote, setGiftNote] = useState();
     return (
         <>
-            <WhoGetTheGift/>
-            <GiftRecipient/>
-            <GiftSender/>
-            <GiftNote/>
-            <AppBar/>
+            <WhoGetTheGift />
+            <GiftRecipient />
+            <GiftSender />
+            <GiftNote />
+            <AppBar />
         </>
     )
 }
