4fc4e3cb690663a10303beee9312d482cf1f147f
mtehila <tehila1633@gmail.com>
2022-01-23 17:20:06 +0200

    set function for the components

--- src/BuyGiftPage.js
+++ src/BuyGiftPage.js
@@ -6,12 +6,28 @@ import GiftSender from './GiftSender'
 import GiftNote from './GiftNote'
 
 export default BuyGiftPage = () => {
+    onWhoGetsTheGiftChange(value)
+    {
+        setWhoGetsTheGift(value);
+    }
+    onGiftRecipientChange(value)
+    {
+        setGiftRecipient(value);
+    }
+    onGiftSenderChange(value)
+    {
+        setGiftSender(value);
+    }
+    onGiftNoteChange(value)
+    {
+        setGiftNote(value);
+    }
     return (
         <>
-            <WhoGetTheGift/>
-            <GiftRecipient/>
-            <GiftSender/>
-            <GiftNote/>
+            <WhoGetTheGift onChange={(event) => onWhoGetsTheGiftChange(event)} />
+            <GiftRecipient onChange={(event) => onGiftRecipientChange(event)} />
+            <GiftSender onChange={(event) => onGiftSenderChange(event)} />
+            <GiftNote onChange={(event) => onGiftNoteChange(event)} />
             <AppBar/>
         </>
     )
