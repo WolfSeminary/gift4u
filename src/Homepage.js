a8ba960db5fb0283bbe187f6153e18643069185e
TamarCoh <96550525+TamarCoh@users.noreply.github.com>
2022-01-23 14:27:31 +0200

    feat(Homepage):put Together homepage (#46)

---
+++ src/Homepage.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(3),
+  paddingLeft:theme.spacing(9),
+  paddingRight:theme.spacing(9),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function HomePage() {
+  return (
+    <div>
+      <Stack direction="row" spacing={2}>
+        <Item><GiftCategories/></Item>
+        <Item>{/*Add <FindGift /> component here*/}</Item>
+      </Stack>
+    </div>
+  );
+}
