acee66e9ed838da7dec502e060551bd1ba2021ef
SariZaretzki <sari127467@gmail.com>
2022-01-23 18:44:49 +0200

    fix:change pages

--- src/BuyGift.js
+++ src/BuyGift.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 
 const BuyGift = () => {
+    const [whoGetsTheGift, setWhoGetsTheGift] = useState("a friend");
+    const [giftRecipient, setGiftRecipient] = useState();
+    const [giftSender, setGiftSender] = useState();
+    const [giftNote, setGiftNote] = useState();
     return <Button id="BuyGift" variant="contained">Buy Gift</Button>
 }
 
--- src/BuyGiftPage.js
+++ src/BuyGiftPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import AppBar from './AppBar'
 import WhoGetTheGift from './WhoGetTheGift'
 import GiftRecipient from './GiftRecipient'
@@ -6,10 +6,6 @@ import GiftSender from './GiftSender'
 import GiftNote from './GiftNote'
 
 export default BuyGiftPage = () => {
-    const [whoGetsTheGift, setWhoGetsTheGift] = useState("a friend");
-    const [giftRecipient, setGiftRecipient] = useState();
-    const [giftSender, setGiftSender] = useState();
-    const [giftNote, setGiftNote] = useState();
     return (
         <>
             <WhoGetTheGift />
