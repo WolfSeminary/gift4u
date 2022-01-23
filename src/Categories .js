b3711a18d5712bf5718b828888d5db8d36bf3e77
tovi4155350 <tovi4155350>
2022-01-23 18:28:06 +0200

    fix Categories page

--- src/Categories .js
+++ src/Categories .js
@@ -1,9 +1,9 @@
 import { Grid } from '@mui/material';
 import React, { useState } from 'react';
 
-export default function Categories([]) {
+export default function Categories() {
 
-    const [list, setList] = useState()
+    const [list, setList] = useState([])
     return <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 8 }}>
         {list.map((cat, i) => (
             <Grid item xs={2} sm={4} md={4}>
