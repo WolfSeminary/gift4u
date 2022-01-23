a9a26eda5cd81942b985964b18ebb48cb38078cf
chaniZilber <chani59942@gmail.com>
2022-01-23 18:49:16 +0200

    feat(Gifts):make the gift clickable

--- src/Gifts.js
+++ src/Gifts.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Grid } from '@mui/material';
+import { useNavigate } from "react-router-dom"; 
 
 export default function Gifts() {
     const [gifts, setgifts] = useState([])
+    const navigate=useNavigate();
+    const onGiftClick=()=>{
+        navigate(`/Categories/${id}`)
+    }
+
     return <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 8 }}>
         {gifts.map((gift, i) => (
             <Grid item xs={2} sm={4} md={4}>
-                <h1>{gift}</h1>
+                <div onClick={onGiftClick}>{gift}</div>
             </Grid>
         ))}
     </Grid>
