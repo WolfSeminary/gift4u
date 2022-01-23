405162a707f52c33569f06efb5202c5957357f43
chaniZilber <chani59942@gmail.com>
2022-01-23 15:06:03 +0200

    alt+shift+f

--- src/GiftSender.js
+++ src/GiftSender.js
@@ -1,21 +1,21 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-const GiftSender=()=> {
+const GiftSender = () => {
   return (
     <>
-    <Label>🟠 Who send the gift?</Label>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic"  variant="outlined" />
-    </Box>
+      <Label>🟠 Who send the gift?</Label>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" variant="outlined" />
+      </Box>
     </>
   );
 }
-export default  GiftSender;
+export default GiftSender;
