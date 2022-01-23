caa90471cd28e9283da22ee73fc1fc1257c5865e
tovi4155350 <tovi4155350>
2022-01-23 17:56:11 +0200

    I fix Gifts page

--- src/Gifts.js
+++ src/Gifts.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid } from '@mui/material';
 
 export default function Gifts() {
-    const [gifts, setgifts] = useState()
+    const [gifts, setgifts] = useState([])
     return <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 8 }}>
         {gifts.map((gift, i) => (
             <Grid item xs={2} sm={4} md={4}>
