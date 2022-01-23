242cd2960c7ff038062c8c0ba3bd58610a41c6a2
SariZaretzki <96003501+SariZaretzki@users.noreply.github.com>
2022-01-23 11:33:43 +0200

    feat(FindGift):add navigate function (#53)

--- src/FindGift.js
+++ src/FindGift.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
 
 const FindGift = () => {
-    return <Button variant="contained">Find a Gift</Button>
+    const navigate = useNavigate();
+    return <Button onClick={()=>navigate('/categories')} variant="contained">Find a Gift</Button>
 }
 
 export default FindGift;
