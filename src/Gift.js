37bbb18547f8351a12a0dcd9163c994a8b0f2828
chaniZilber <chani59942@gmail.com>
2022-01-23 19:18:08 +0200

    feat(gift,gifts,giftDetailed):pass the info between the component

--- src/Gift.js
+++ src/Gift.js
@@ -5,7 +5,7 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
-export default function Gift() {
+export default function Gift({info}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
