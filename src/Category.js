d3fc76a355a6cae2d7357eccdbf954d7f7f14fc4
yocheved1000 <co5703959@gmail.com>
2022-01-23 12:10:38 +0200

    add category component

---
+++ src/Category.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+export default function Category() {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    food
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+}
