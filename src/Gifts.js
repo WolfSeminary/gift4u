13301801ebefe6482e3c37d9460dca096b31b7d8
chaniZilber <chani59942@gmail.com>
2022-01-23 19:35:28 +0200

    fix:change tag

--- src/Gifts.js
+++ src/Gifts.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Grid } from '@mui/material';
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
+
 
 export default function Gifts() {
     const [gifts, setgifts] = useState([])
-    const navigate=useNavigate();
-    const onGiftClick=()=>{
+    const navigate = useNavigate();
+    const onGiftClick = () => {
         navigate(`/Categories/${id}`)
     }
 
     return <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 8 }}>
         {gifts.map((gift, i) => (
             <Grid item xs={2} sm={4} md={4}>
-                <div onClick={onGiftClick}>{gift}</div>
+                <div onClick={onGiftClick}><Gift /></div>
             </Grid>
         ))}
     </Grid>
