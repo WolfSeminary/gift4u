03de802e50b7cfc9c85176dc3538af6b507623ed
chaniZilber <chani59942@gmail.com>
2022-01-23 16:19:48 +0200

    makeTheCategoryItemClickable

--- src/CategoriesPage.js
+++ src/CategoriesPage.js
@@ -1,11 +1,18 @@
 import AppBar from '/AppBar'
 import Categories from './Categories'
+import { useNavigate } from "react-router-dom";
 
 export default CategoriesPage = () => {
+    
+    const onCategoryClick = () => {
+        navigate(`/Categories/${newInvoice.id}`)
+    }
+
+    const navigate = useNavigate();
     return (
         <>
             <AppBar/>
-            <Categories/>
+            <div><Categories onClick={onCategoryClick} /></div>
         </>
     )
 }
