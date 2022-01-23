9aa2baae20792404ab7058fbbcfd717956fdd415
CohenAyala <96003746+CohenAyala@users.noreply.github.com>
2022-01-23 11:57:29 +0200

    addPropsToGiftDetailed (#51)

--- src/BuyGift.js
+++ src/BuyGift.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+
 const BuyGift=()=>{
     return  <Button id="BuyGift" variant="contained">Buy Gift</Button>
 }
+
 export default BuyGift;
--- src/GiftDetailed.js
+++ src/GiftDetailed.js
@@ -1,21 +1,21 @@
 import * as React from 'react';
 import { Card, CardHeader, CardMedia, CardContent, CardActions, Button, Typography, Box } from '@mui/material';
 
-const GiftDetailed = () => {
+const GiftDetailed = (props) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <Typography component="div">
         <Box sx={{ textAlign: 'right', m: 1 }}>
-          <CardHeader title="gift price" /></Box>
+          <CardHeader title={props.price} /></Box>
       </Typography>
       <CardMedia
         component=""
         height="194"
-        image="url"
+        image={props.image}
         alt="gift image"
       />
       <CardContent>
-        <Typography variant="body2" color="text.secondary">gift name
+        <Typography variant="body2" color="text.secondary">{props.name}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
