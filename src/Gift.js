4ad170d69fae574504018dbaf99ed86f8538d337
Tovi0504155350 <tovi4155350@gmail.com>
2022-01-25 23:30:31 +0200

    feat(Gift): Breadcrumbs component - Navigation on click

--- src/Gift.js
+++ src/Gift.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { CardActionArea,Card,CardMedia,Typography,CardContent } from '@mui/material';
+import { CardActionArea, Card, CardMedia, Typography, CardContent, Breadcrumbs} from '@mui/material';
+import { Link } from 'react-router-dom';
 
-export default function Gift({info}) {
+export default function Gift({ info }) {
+
+  const routing = [
+    <Link to="/Categories">Categories</Link>,
+    <Link to="/CategoriesPage">{info.categoryName}</Link>,
+    <Link to="/Gift">{info.giftName}</Link>
+  ];
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" >
-            Lizard
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <>    
+        <Breadcrumbs separator=">" aria-label="breadcrumbs">
+          {routing}
+        </Breadcrumbs>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" >
+              Lizard
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </>
   );
 }
