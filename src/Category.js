b7512d5eb53054431eaae629958e731cd639fa6c
yocheved1000 <96573084+yocheved1000@users.noreply.github.com>
2022-01-25 22:45:53 +0200

    add category component (#58)
    Co-authored-by: eini-rc <REini@madisoncres.com>

--- src/Category.js
+++ src/Category.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 export default function Category({ info }) {
-
-    return (<>
-        <h1>{info.CategoryName}</h1>
-        <img src={info.categoryImage} />
-    </>)
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={info.categoryImage}
+        alt={info.categoryImage}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.CategoryName}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
-
