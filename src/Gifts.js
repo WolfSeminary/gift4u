34545a79557f7d46676ce0a2ff0870422b1ff4ae
chaniZilber <chani59942@gmail.com>
2022-01-25 18:34:28 +0200

    fix

--- src/Gifts.js
+++ src/Gifts.js
@@ -4,7 +4,7 @@ import Gift from './Gift';
 import { useNavigate } from "react-router-dom"; 
 
 export default function Gifts() {
-    const [gifts, setgifts] = useState([
+    const [gifts, setGifts] = useState([
         { GiftID: 11, categoryID: 1, giftName: 'Brunch', giftImage: './public/img/Brunch.jpg' },
         { GiftID: 12, categoryID: 1, giftName: 'Lunch', giftImage: './public/img/Lunch.jpg' },
         { GiftID: 21, categoryID: 2, giftName: 'Zoo', giftImage: './public/img/Zoo1.jpg' },
@@ -15,13 +15,13 @@ export default function Gifts() {
         { GiftID: 42, categoryID: 4, giftName: 'Lego', giftImage: './public/img/lego.jpg' }
     ]);
     const navigate = useNavigate();
-    const onGiftClick = ({id}) => {
-        navigate(`/Categories/${id}`)
+    const onGiftClick = ({giftname}) => {
+        navigate(`/Gifts/${giftname}`)
     }
     return <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 8 }}>
         {gifts.map((gift, i) => (
             <Grid item xs={2} sm={4} md={4}>
-                 <div onClick={onGiftClick}><Gift /></div>
+                <Gift onClick={onGiftClick}/>
             </Grid>
         ))}
     </Grid>
