72a216d2db1a4596f2bed6f54bf85f2e52c1ded8
SariZaretzki <96003501+SariZaretzki@users.noreply.github.com>
2022-01-25 18:39:46 +0200

    feat(Gifts):make the gift clickable (#69)
    * feat(Gifts):make the gift clickable
    
    * fix:change tag
    
    * fix:variable
    
    * fix
    
    Co-authored-by: chaniZilber <chani59942@gmail.com>

--- src/Gifts.js
+++ src/Gifts.js
@@ -1,11 +1,10 @@
 import React, { useState, useParams } from 'react';
 import { Grid } from '@mui/material';
 import Gift from './Gift';
+import { useNavigate } from "react-router-dom"; 
 
 export default function Gifts() {
-    const [categoryId, setategoryId] = useState();
-    setategoryId(useParams());
-    const [gifts, setgifts] = useState([
+    const [gifts, setGifts] = useState([
         { GiftID: 11, categoryID: 1, giftName: 'Brunch', giftImage: './public/img/Brunch.jpg' },
         { GiftID: 12, categoryID: 1, giftName: 'Lunch', giftImage: './public/img/Lunch.jpg' },
         { GiftID: 21, categoryID: 2, giftName: 'Zoo', giftImage: './public/img/Zoo1.jpg' },
@@ -15,11 +14,14 @@ export default function Gifts() {
         { GiftID: 41, categoryID: 4, giftName: 'Playmobil', giftImage: './public/img/playmobil.jpg' },
         { GiftID: 42, categoryID: 4, giftName: 'Lego', giftImage: './public/img/lego.jpg' }
     ]);
-    setgifts(gifts.filter(gift => gift.categoryID == categoryId));
+    const navigate = useNavigate();
+    const onGiftClick = ({giftname}) => {
+        navigate(`/Gifts/${giftname}`)
+    }
     return <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 8 }}>
         {gifts.map((gift, i) => (
             <Grid item xs={2} sm={4} md={4}>
-                <Gift info={gift} />
+                <Gift onClick={onGiftClick}/>
             </Grid>
         ))}
     </Grid>
