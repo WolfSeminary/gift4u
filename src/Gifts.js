073721b27ef7646b05a94e7265b0c871e604f9c4
CohenAyala <ayala40565@gmail.com>
2022-01-25 18:22:57 +0200

    fix:filter the gifts

--- src/Gifts.js
+++ src/Gifts.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useParams } from 'react';
 import { Grid } from '@mui/material';
 import Gift from './Gift';
 
 export default function Gifts() {
+    const [categoryId, setategoryId] = useState();
+    setategoryId(useParams());
     const [gifts, setgifts] = useState([
         { GiftID: 11, categoryID: 1, giftName: 'Brunch', giftImage: './public/img/Brunch.jpg' },
         { GiftID: 12, categoryID: 1, giftName: 'Lunch', giftImage: './public/img/Lunch.jpg' },
@@ -13,11 +15,11 @@ export default function Gifts() {
         { GiftID: 41, categoryID: 4, giftName: 'Playmobil', giftImage: './public/img/playmobil.jpg' },
         { GiftID: 42, categoryID: 4, giftName: 'Lego', giftImage: './public/img/lego.jpg' }
     ]);
-
+    setgifts(gifts.filter(gift => gift.categoryID == categoryId));
     return <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 8 }}>
         {gifts.map((gift, i) => (
             <Grid item xs={2} sm={4} md={4}>
-                <Gift info={gift}/>
+                <Gift info={gift} />
             </Grid>
         ))}
     </Grid>
